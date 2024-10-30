dcc7018bf107171c55d2798043c503ffe8921679
augustine-a8 <logos.agbeko@gmail.com>
2024-10-30 13:38:13 +0000

    typeorm connection issue

--- backend/src/@types/env.d.ts
+++ backend/src/@types/env.d.ts
@@ -1,11 +1,10 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-        DATABASE_TYPE: "mysql" | "mariadb" | "postgres" | "cockroachdb" | "sqlite" | "mssql" | "sap" | "oracle" | "cordova" | "nativescript" | "react-native" | "sqljs" | "mongodb" | "aurora-mysql",
-        DATABASE_HOST: string,
-        DATABASE_PORT: number,
-        DATABASE_USERNAME: string,
-        DATABASE_PASSWORD: string,
-        DATABASE_NAME: string,
-    }
+  interface ProcessEnv {
+    DATABASE_HOST: string;
+    DATABASE_PORT: string;
+    DATABASE_USERNAME: string;
+    DATABASE_PASSWORD: string;
+    DATABASE_NAME: string;
+    PORT: string;
   }
-  
+}
