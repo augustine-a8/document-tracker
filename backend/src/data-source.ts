e47b2df165e0377d8c27aa43e121385a77c73c6e
augustine-a8 <logos.agbeko@gmail.com>
2024-10-30 12:14:38 +0000

    create datasource for initializing db connection

---
+++ backend/src/data-source.ts
@@ -0,0 +1,26 @@
+import { DataSource } from "typeorm";
+
+import { User, Document, CustodyHistory } from "./entity";
+import { Config } from "./config";
+
+const {
+  database_host,
+  database_name,
+  database_password,
+  database_port,
+  database_user,
+} = Config;
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: database_host,
+  port: database_port,
+  username: database_user,
+  password: database_password,
+  database: database_name,
+  synchronize: true,
+  logging: true,
+  entities: [User, Document, CustodyHistory],
+  subscribers: [],
+  migrations: [],
+});
