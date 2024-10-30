3689ac7538cd84dbc985c900edf7dab4b0e1119a
augustine-a8 <logos.agbeko@gmail.com>
2024-10-30 18:28:58 +0000

    add new column to document table

--- backend/src/entity/Document.ts
+++ backend/src/entity/Document.ts
@@ -6,13 +6,16 @@ export class Document extends BaseEntity {
     @PrimaryColumn({type: "uuid"})
     document_id: string
 
-    @Column({type: "varchar2", length: 64})
+    @Column({type: "varchar", length: 64, nullable: false})
     title: string
 
-    @Column({type: "varchar2", length: 255})
+    @Column({type: "varchar", length: 255, nullable: false})
     description: string
 
-    @Column({type: "varchar2", enum: ["available", "assigned"], length: 50, default: "available"})
+    @Column({type: "varchar", unique: true, nullable: false})
+    serialNumber: string
+
+    @Column({type: "varchar", enum: ["available", "assigned"], length: 50, default: "available"})
     status: string
 
     @OneToOne(() => User)
