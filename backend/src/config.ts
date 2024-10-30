f9f14188026cbd55e0197d95b908ac522a421294
augustine-a8 <logos.agbeko@gmail.com>
2024-10-30 18:27:44 +0000

    remove database type as env variable

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,10 @@
 const Config = {
-    database_host: process.env.DATABASE_HOST,
-    database_name: process.env.DATABASE_NAME,
-    database_password: process.env.DATABASE_NAME,
-    database_user: process.env.DATABASE_USERNAME,
-    database_port: process.env.DATABASE_PORT
-}
+  database_host: process.env.DATABASE_HOST,
+  database_name: process.env.DATABASE_NAME,
+  database_password: process.env.DATABASE_PASSWORD,
+  database_user: process.env.DATABASE_USERNAME,
+  database_port: parseInt(process.env.DATABASE_PORT as string, 10) || 5432,
+  port: parseInt(process.env.PORT as string, 10) || 8888,
+};
 
-export {Config}
+export { Config };
