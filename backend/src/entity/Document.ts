99f40f71dd315c1f9113cb1a9b23996a9977a233
augustine-a8 <logos.agbeko@gmail.com>
2024-10-31 04:06:24 +0000

    fixes serialNumber column name

--- backend/src/entity/Document.ts
+++ backend/src/entity/Document.ts
@@ -1,24 +1,36 @@
-import {PrimaryColumn, Column, Entity, BaseEntity, OneToOne, JoinColumn} from 'typeorm'
-import { User } from './User'
+import {
+  PrimaryColumn,
+  Column,
+  Entity,
+  BaseEntity,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./User";
 
 @Entity()
 export class Document extends BaseEntity {
-    @PrimaryColumn({type: "uuid"})
-    document_id: string
+  @PrimaryColumn({ type: "uuid" })
+  document_id: string;
 
-    @Column({type: "varchar", length: 64, nullable: false})
-    title: string
+  @Column({ type: "varchar", length: 64, nullable: false })
+  title: string;
 
-    @Column({type: "varchar", length: 255, nullable: false})
-    description: string
+  @Column({ type: "varchar", length: 255, nullable: false })
+  description: string;
 
-    @Column({type: "varchar", unique: true, nullable: false})
-    serialNumber: string
+  @Column({ type: "varchar", unique: true, nullable: false })
+  serial_number: string;
 
-    @Column({type: "varchar", enum: ["available", "assigned"], length: 50, default: "available"})
-    status: string
+  @Column({
+    type: "varchar",
+    enum: ["available", "assigned"],
+    length: 50,
+    default: "available",
+  })
+  status: string;
 
-    @OneToOne(() => User)
-    @JoinColumn()
-    current_holder: User
+  @OneToOne(() => User)
+  @JoinColumn({ name: "current_holder_id" })
+  current_holder: User;
 }
