5a6787d468d28bf8674dcfe23778946b4e69baf8
augustine-a8 <logos.agbeko@gmail.com>
2024-11-01 10:17:03 +0000

    adds typing to config object

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,13 @@
-const Config = {
-  database_host: process.env.DATABASE_HOST,
-  database_name: process.env.DATABASE_NAME,
-  database_password: process.env.DATABASE_PASSWORD,
-  database_user: process.env.DATABASE_USERNAME,
+import { IConfig } from "./@types/config";
+
+const Config: IConfig = {
+  database_host: process.env.DATABASE_HOST as string,
+  database_name: process.env.DATABASE_NAME as string,
+  database_password: process.env.DATABASE_PASSWORD as string,
+  database_user: process.env.DATABASE_USERNAME as string,
   database_port: parseInt(process.env.DATABASE_PORT as string, 10) || 5432,
   port: parseInt(process.env.PORT as string, 10) || 8888,
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: process.env.JWT_SECRET as string,
 };
 
 export { Config };
