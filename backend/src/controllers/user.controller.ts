8992d219792ca4c160f05876bad369ee645da7ec
augustine-a8 <logos.agbeko@gmail.com>
2024-11-02 08:51:23 +0000

    adds getMyProfile and searchUserByNameAndEmail

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,47 +1,91 @@
-import {Request, Response} from 'express'
-import {v4 as uuidv4} from 'uuid'
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 
-import { AppDataSource } from '../data-source'
-import { User } from '../entity'
+import { AppDataSource } from "../data-source";
+import { User } from "../entity";
+import { AuthRequest } from "../@types/authRequest";
 
-const UserRepository = AppDataSource.getRepository(User)
+const UserRepository = AppDataSource.getRepository(User);
 
 async function getAllUsers(req: Request, res: Response) {
-    const allUsers = await UserRepository.find({})
+  const allUsers = await UserRepository.find({});
 
-    res.status(200).json({
-        message: "All users retrieved",
-        allUsers
-    })
+  res.status(200).json({
+    message: "All users retrieved",
+    allUsers,
+  });
 }
 
 async function addUser(req: Request, res: Response) {
-    const {name, email} = req.body
+  const { name, email } = req.body;
 
-    if (!name) {
-        res.status(400).json({
-            message: "All users should have name provided"
-        })
-        return
-    }
-    
-    if (!email) {
-        res.status(400).json({
-            message: "All users should have email provided"
-        })
-        return 
-    }
+  if (!name) {
+    res.status(400).json({
+      message: "All users should have name provided",
+    });
+    return;
+  }
 
-    const user = new User()
-    user.user_id = uuidv4()
-    user.name = name
-    user.email = email
+  if (!email) {
+    res.status(400).json({
+      message: "All users should have email provided",
+    });
+    return;
+  }
 
-    await UserRepository.save(user)
+  const user = new User();
+  user.userId = uuidv4();
+  user.name = name;
+  user.email = email;
 
-    res.status(200).json({
-        message: "New user created"
-    })
+  await UserRepository.save(user);
+
+  res.status(200).json({
+    message: "New user created",
+  });
 }
 
-export {getAllUsers, addUser}
+async function searchUserByNameOrEmail(req: AuthRequest, res: Response) {
+  const { searchParam } = req.body;
+
+  if (!searchParam) {
+    res.status(400).json({
+      message: "Enter what to search for",
+    });
+    return;
+  }
+
+  const users = await UserRepository.find({});
+  const searchResults = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchParam.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchParam.toLowerCase())
+  );
+
+  res.status(200).json({
+    message: "Search results returned",
+    searchResults: searchResults.filter(
+      (user) => user.userId !== req.user.userId
+    ),
+  });
+}
+
+async function getMyAccount(req: AuthRequest, res: Response) {
+  const user = req.user;
+  const myAccount = await UserRepository.findOne({
+    where: { userId: user.userId },
+  });
+  if (!myAccount) {
+    res.status(404).json({
+      message: "User not found",
+    });
+    return;
+  }
+
+  res.status(200).json({
+    message: "User found",
+    myAccount,
+  });
+}
+
+export { getAllUsers, addUser, searchUserByNameOrEmail, getMyAccount };
