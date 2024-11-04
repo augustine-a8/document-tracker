b9f06347db87c350cabae6680136a5607bdca95a
augustine-a8 <logos.agbeko@gmail.com>
2024-11-04 11:38:42 +0000

    adds new type for jwt auth management

--- backend/src/@types/authRequest.d.ts
+++ backend/src/@types/authRequest.d.ts
@@ -3,3 +3,9 @@ import { Request } from "express";
 export interface AuthRequest extends Request {
   user?: any;
 }
+
+export interface IAuthUser {
+  userId: string;
+  email: string;
+  role: string;
+}
