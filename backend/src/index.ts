604db74b06ea4117d9425d691590ef976f1e52a8
augustine-a8 <logos.agbeko@gmail.com>
2024-11-04 11:36:45 +0000

    adds socket server to http server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,40 @@
 import "reflect-metadata";
 import http from "http";
 import { config as dotenvConfig } from "dotenv";
+import { Server } from "socket.io";
 
 import { createServer } from "./server";
 import { Config } from "./config";
 import { AppDataSource } from "./data-source";
+import { SocketService } from "./services/SocketService";
 
 async function initializeServer() {
   dotenvConfig();
-  const { port } = Config;
+  const { port, jwtSecret } = Config;
 
   const app = createServer();
   const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
 
-    AppDataSource.initialize()
-      .then(async () => {
-        console.log("Database connection successful");
-        await new Promise<void>((resolve) => {
-          httpServer.listen({ port }, resolve);
-        });
-        console.log(`Server is running at http://localhost:${port}`);
-      })
-      .catch((err) => {
-        console.log("Database connection failed");
-        console.log(`${err}`);
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log("Database connection successful");
+      SocketService.getInstance(io);
+      await new Promise<void>((resolve) => {
+        httpServer.listen({ port }, resolve);
       });
+      console.log(`Server is running at http://localhost:${port}`);
+    })
+    .catch((err) => {
+      console.log("Database connection failed");
+      console.log(`${err}`);
+    });
   // await new Promise<void>((resolve) => {
   //   httpServer.listen({ port }, resolve);
   // });
